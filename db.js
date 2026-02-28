3e591ed7c44ce810d6fc4d56790c0a2a9c396bf2
israr khan <israrsql@gmail.com>
2026-02-28 15:19:34 +0530

    adding databaseURL

--- db.js
+++ db.js
@@ -2,11 +2,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.PG_HOST || "localhost",
-  port: process.env.PG_PORT || 5432,
-  user: process.env.PG_USER || "postgres",
-  password: process.env.PG_PASSWORD || 171982,
-  database: process.env.PG_DB || "wealthwave",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
